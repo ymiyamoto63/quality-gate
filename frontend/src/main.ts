5ec3469ca510285908b3d4f372750542ca9ae4be
ymiyamoto63 <miyamooo63@gmail.com>
2026-09-22 16:03:13 +0900

    primevueのバージョン下げ（ライセンス関連）

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
+import Aura from '@primevue/themes/aura'
 import App from './App.vue'
 import { router } from './router'
 
